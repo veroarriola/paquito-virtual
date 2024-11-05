b4e86a34c195a85933b9a5fccaa56d93381902eb
veroarriola <v.arriola@ciencias.unam.mx>
2024-11-05 00:11:59 -0600

    Structure for simple world

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # paquito-virtual
 Gazebo virtual work with a paquito robot
+
+# Packages
+
+Created with:
+```
+ros2 pkg create --build-type ament_cmake --license GPL-3.0-only virtual_paquito
+```
+
---
+++ paquito_models/launch/simpleworld.launch.py
@@ -0,0 +1,42 @@
+"""Launch Gazebo server and client with command line arguments."""
+
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+
+models_package = 'paquito_models'
+
+os.environ['GZ_SIM_RESOURCE_PATH'] = \
+    os.path.join( get_package_share_directory(models_package),
+        'worlds'
+    ) + \
+    os.pathsep + os.path.join(
+        get_package_share_directory(models_package),
+        'models'
+    )
+
+def generate_launch_description():
+    verbose = LaunchConfiguration('v', default='1')
+    pkg_launch_arg = DeclareLaunchArgument(
+        'v',
+        default_value='1'
+    )
+
+    world_file_name = 'simple-world.sdf'
+
+    world = os.path.join(
+        get_package_share_directory(models_package),
+        'worlds',
+        world_file_name
+    )
+
+    return LaunchDescription([
+        pkg_launch_arg,
+        ExecuteProcess(
+            cmd=['gz', 'sim', '-v', verbose, world_file_name],
+            output='screen'),
+    ])
+
